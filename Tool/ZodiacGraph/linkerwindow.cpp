4e5c9a143dc7dfaa5ab5e6a533614c2f1dff94d0
Chris Ferguson <fergyc@hotmail.com>
2017-10-05 20:26:23 +0200

    Added scroller to linker window

--- Tool/ZodiacGraph/linkerwindow.cpp
+++ Tool/ZodiacGraph/linkerwindow.cpp
@@ -1,6 +1,6 @@
 #include "linkerwindow.h"
 
-#include <QDebug>
+#include <QScrollArea>
 
 LinkerWindow::LinkerWindow(zodiac::NodeHandle &node, QList<zodiac::NodeHandle> &nodeList, QWidget *parent)
     : QDialog(parent)
@@ -13,13 +13,18 @@ LinkerWindow::LinkerWindow(zodiac::NodeHandle &node, QList<zodiac::NodeHandle> &
         {
             if((*nodeIt).getType() == zodiac::NODE_NARRATIVE)
             {
-                qDebug() << "narrative:" << (*nodeIt).getName();
-                m_narrativeNodeList.push_back((*nodeIt));
+                if((*nodeIt).getName() != "SEQ" && (*nodeIt).getName() != "SEQ")
+                    m_narrativeNodeList.push_back((*nodeIt));
             }
             else
                 if((*nodeIt).getType() == zodiac::NODE_STORY)
                 {
-                    qDebug() << "story:" << (*nodeIt).getName();
+                    if((*nodeIt).getStoryNodeType() != zodiac::STORY_NAME && (*nodeIt).getStoryNodeType() != zodiac::STORY_SETTING && (*nodeIt).getStoryNodeType() != zodiac::STORY_SETTING_CHARACTER_GROUP &&
+                            (*nodeIt).getStoryNodeType() != zodiac::STORY_SETTING_LOCATION_GROUP && (*nodeIt).getStoryNodeType() != zodiac::STORY_SETTING_TIME_GROUP && (*nodeIt).getStoryNodeType() != zodiac::STORY_THEME&&
+                            (*nodeIt).getStoryNodeType() != zodiac::STORY_THEME_EVENT_GROUP && (*nodeIt).getStoryNodeType() != zodiac::STORY_THEME_GOAL_GROUP && (*nodeIt).getStoryNodeType() != zodiac::STORY_PLOT &&
+                            (*nodeIt).getStoryNodeType() != zodiac::STORY_PLOT_EPISODE_ATTEMPT_GROUP && (*nodeIt).getStoryNodeType() != zodiac::STORY_PLOT_EPISODE_OUTCOME_GROUP &&
+                            (*nodeIt).getStoryNodeType() != zodiac::STORY_RESOLUTION && (*nodeIt).getStoryNodeType() != zodiac::STORY_RESOLUTION_EVENT_GROUP &&
+                            (*nodeIt).getStoryNodeType() != zodiac::STORY_RESOLUTION_STATE_GROUP)
                     m_storyNodeList.push_back((*nodeIt));
                 }
         }
@@ -38,7 +43,7 @@ LinkerWindow::LinkerWindow(zodiac::NodeHandle &node, QList<zodiac::NodeHandle> &
     mainLayout->addWidget(m_cancelBtn);
     setLayout(mainLayout);
 
-    setWindowTitle(tr("Link Nodes"));
+    setWindowTitle(tr("Link Nodes to ") + node.getName());
 }
 
 NodeTab::NodeTab(QList<zodiac::NodeHandle> nodeList, zodiac::NodeType nodeType, QWidget *parent)
@@ -47,13 +52,36 @@ NodeTab::NodeTab(QList<zodiac::NodeHandle> nodeList, zodiac::NodeType nodeType, 
     , m_nodeType(nodeType)
 {
     QVBoxLayout *mainLayout = new QVBoxLayout;
+    QScrollArea *scrollArea = new QScrollArea;
+    QGridLayout *nodeLayout = new QGridLayout;
+
+    int x = 0;
+    int y = 0;
 
     for(QList<zodiac::NodeHandle>::iterator nodeIt = nodeList.begin(); nodeIt != nodeList.end(); ++nodeIt)
     {
         QCheckBox *checkBox = new QCheckBox((*nodeIt).getName());
         checkboxes.push_back(checkBox);
-        mainLayout->addWidget(checkBox);
+        nodeLayout->addWidget(checkBox, x, y);
+
+        ++x;
+
+        if(x > 12)
+        {
+            x = 0;
+            ++y;
+        }
     }
 
+    nodeLayout->setSizeConstraint(QLayout::SetFixedSize);
+    QWidget *widg = new QWidget();
+    widg->setLayout(nodeLayout);
+    scrollArea->setWidget(widg);
+
+    nodeLayout->setAlignment(Qt::AlignTop);
+
+    mainLayout->addWidget(scrollArea);
     setLayout(mainLayout);
+
+
 }
