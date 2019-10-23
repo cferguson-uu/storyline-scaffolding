a6f20fd92139736f9ae39be63d2d38277a68738f
Chris Ferguson <fergyc@hotmail.com>
2019-10-23 23:13:33 +0200

    Started to implement loading in spatial graph for automatic lostness calculations

--- Tool/ZodiacGraph/lostness.cpp
+++ Tool/ZodiacGraph/lostness.cpp
@@ -29,3 +29,37 @@ float Lostness::getLostnessValue(int minSteps, int totalSteps, int uniqueSteps)
     qDebug() << "Lostness" << lostness;
     return lostness;
 }
+
+void Lostness::loadSpatialGraph()
+{
+    QFile file(QFileDialog::getOpenFileName(this,
+                                                     QObject::tr("Load Spatial Graph"), "",
+                                                     QObject::tr("JSON File (*.json);;All Files (*)")));
+
+    if(!file.fileName().isEmpty() && !file.fileName().isNull())
+    {
+        if(file.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            QString settings = file.readAll();
+            file.close();
+
+            QJsonDocument jsonDoc = QJsonDocument::fromJson(settings.toUtf8());
+            QJsonObject jsonObj = jsonDoc.object();
+            QJsonArray jsonEdgesArray = jsonObj["edges"].toArray();
+
+            foreach (const QJsonValue &v, jsonEdgesArray)
+            {
+                QJsonArray jsonLinksArray = v.toArray();
+
+                QString first =  jsonLinksArray[0].toString();
+                QString second = jsonLinksArray[1].toString();
+
+                //if node zero not in vector/whatever, add it
+                // if node one not in vector/whatever, add it
+                //add node one to node 0 node list
+                //Breadth-first search - https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
+            }
+
+        }
+    }
+}
--- Tool/ZodiacGraph/lostness.h
+++ Tool/ZodiacGraph/lostness.h
@@ -1,15 +1,22 @@
 #ifndef LOSTNESS_H
 #define LOSTNESS_H
 
+#include <QObject>
 #include <QDebug>
+#include <QFileDialog>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
 
 
-class Lostness
+class Lostness : public QWidget
 {
 public:
     Lostness();
 
     float getLostnessValue(int minSteps, int totalSteps, int uniqueSteps);
+
+    void loadSpatialGraph();
 };
 
 #endif // LOSTNESS_H
